6b35a40a2872ce5fbca1a2dd978ece547527a97a
Anmol Goyal <anmolafk7@gmail.com>
2026-06-02 20:31:09 +0530

    Delete step-01-basics/things-to-know/loops directory

--- step-01-basics/things-to-know/loops/loop5.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-int main() {
-    for (int i = 5; i > 0 ; i--) {
-        for (int j = i; j> 0; j--) {
-            std::cout << "*";
-        }
-        std::cout << std::endl;
-    }
-}
