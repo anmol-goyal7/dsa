ceb248714ce16a90ead97c0e5c8c87d674089cf2
anmol <anmolafk7@gmail.com>
2026-06-05 17:55:05 +0530

    reorganised the repo and added pattern9.cpp

--- 01-basics/patterns/pattern9.cpp
+++ 01-basics/patterns/pattern9.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 int main() {
+     for (int i = 0; i < 5 ; i++) {
+        for (int k = 0; k <4 - i;k++) {
+            std::cout << " ";
+        } 
+
+        for (int j = 1; j<=(i*2)+1 ; j++) {
+
+            std::cout << "*";
+        }
+      std::cout << '\n';
+    }
+ 
     for (int i = 4; i >= 0 ; i--) {
         for (int k = 0; k< 4 - i;k++) {
             std::cout << " ";
