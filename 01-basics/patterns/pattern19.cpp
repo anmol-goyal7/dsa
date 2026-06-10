cc66d6cfd008d3128fd96c1ba374c48879a0391a
anmol <anmolafk7@gmail.com>
2026-06-10 16:34:05 +0530

    commit

---
+++ 01-basics/patterns/pattern19.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+int main() {
+     for (int i = 5; i >=1; i--) {
+        for (int k = 5; k>=i;k--) {
+            std::cout << "*";
+        } 
+        for (int j = 0; j<  ; j++) {
+            std::cout << " ";
+        }
+        for (int l = 0; l<  ; l++) {
+            std::cout << "*";
+        }
+      std::cout << '\n';
+}
+
+for (int i = 5; i >=1; i--) {
+        for (int k = 5; k>=i;k--) {
+            std::cout << "*";
+        } 
+        for (int j = 0; j<  ; j++) {
+            std::cout << " ";
+        }
+        for (int l = 0; l<  ; l++) {
+            std::cout << "*";
+        }
+      std::cout << '\n';
+
