197b69f1bc9a4c1202ec168e6ab9a82c31fdcc31
anmol <anmolafk7@gmail.com>
2026-07-25 22:34:11 +0530

    counting the digits

--- 01-basics/digits/extract_digits.cpp
+++ 01-basics/digits/extract_digits.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 int main() {
-    int n, w, last_digit;
+    int n, last_digit, count{0};
     std::cin >> n;
-    
+     
     while (n>0) {
         last_digit = n % 10;
-        
+        count = count + 1;
         n = n/10;
         
         std::cout << last_digit;
     }
+    std::cout << std::endl;
+    std::cout << count << std::endl;
 }
