ebbf6baaae7b4d59a73a905a032923f39f006e67
anmol <anmolafk7@gmail.com>
2026-08-04 10:56:08 +0530

    sorting

--- 01-basics/digits/factors.cpp
+++ 01-basics/digits/factors.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
+#include <algorithm>
 
 int main() {
     int n;
     std::cin >> n;
+    std::vector<int> ls;
     for (int i = 1; i <= sqrt(n); i++ )
     {
         if (n%i== 0) {
-            std::cout << i << "\n";
-            if ((n/i)!= i)
-                std::cout << (n/i) << "\n";
+            ls.push_back(i);
+            if ((n/i)!= i) {
+                ls.push_back(n/i);
+            }
         }
     }
+    std::sort(ls.begin(), ls.end());
+    for(auto it : ls) std::cout << it << " ";
 }
