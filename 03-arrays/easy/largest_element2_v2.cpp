88d95c06993f2b1f1db457c7ab61789800f0aa1c
anmol <anmolafk7@gmail.com>
2026-08-23 01:37:23 +0530

    remove the broken largest element draft

--- 03-arrays/easy/largest_element2_v2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-int main() {
-  int largest{0};
-  int arr[] = {3, 2, 5, 1, 5, 6, 9} for (int i = 0;
-                                         i < sizeof(arr) / sizeof(arr[1]);
-                                         i++) if (arr[i] > largest) largest =
-      arr[i];
-  std::cout << largest;
-}
